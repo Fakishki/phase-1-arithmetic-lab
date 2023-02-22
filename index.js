01c96e11c21f97fc90d3d904884d568bcb513056
Wally Pfingsten <wallypfingsten@gmail.com>
2023-02-21 21:27:34 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
 
-// Write your code here
+let num2 = 2;
+
+let multiply = (num1 * num2);
+
+let random = (Math.ceil(Math.random() * 10) + 1);
+
+let num3 = 104;
+
+let num4 = 25;
+
+let mod = (num3 % num4);
+
+let max = Math.max(1, 3, 20, 9, 15)
